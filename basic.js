436605c159d97ef8a0a432055732524799599e4d
Harsh Panwar <harshpanwar462@gmail.com>
2024-08-28 22:46:26 +0530

    let const declaration

--- basic.js
+++ basic.js
@@ -1,2 +1,21 @@
 // to print
-console.log("")
+console.log("Hi")
+
+// to store data in memory we use variable and constant to identify the data with a name
+// to declear the constant
+const accountId=1
+
+// to delear variable
+let mail = "harsh123@gmail.com"
+var password = "12345"
+city = "Indore"
+
+// to give multiple output at same time in a table formate
+console.table([mail,password,city])
+
+/* do not use var beacuse it gives issue in scope{} "for suppose if i have declared a variable named password 
+and by mistake i write another variable with same name inside a scope, so it will change the value of my main
+password variable which should not be happening according to global variable concept."
+*/
+
+// always use const and let
